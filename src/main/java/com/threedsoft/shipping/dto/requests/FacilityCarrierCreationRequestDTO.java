39f3e6b92c9159f00a3a06a69f2b61eed7516250
Krishna Pillutla <knpillutla@gmail.com>
2018-12-27 22:18:24 -0500

    Adding

---
+++ src/main/java/com/threedsoft/shipping/dto/requests/FacilityCarrierCreationRequestDTO.java
@@ -0,0 +1,26 @@
+package com.threedsoft.shipping.dto.requests;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.threedsoft.util.dto.WMSRequestDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class FacilityCarrierCreationRequestDTO extends WMSRequestDTO{
+	String busName;
+	Integer locnNbr;
+	String carrierCode;
+	String accountNbr;
+}
+
---
+++ src/main/java/com/threedsoft/shipping/dto/responses/FacilityCarrierDTO.java
@@ -0,0 +1,34 @@
+package com.threedsoft.shipping.dto.responses;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.threedsoft.util.dto.WMSResourceDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class FacilityCarrierDTO  extends WMSResourceDTO implements Serializable{
+	Long id;
+	String busName;
+	Integer locnNbr;
+	String carrierCode;
+	String accountNbr;
+	String source;
+	String transactionName;
+	String refField1;
+	String refField2;
+	LocalDateTime updatedDttm;
+	String updatedBy;
+}
